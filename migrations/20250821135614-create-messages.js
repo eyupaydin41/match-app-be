424edc2b0126ea4880475ca866125eef1ccc0786
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2025-08-22 01:50:59 +0300

    feat: create messages table

---
+++ migrations/20250821135614-create-messages.js
@@ -0,0 +1,47 @@
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("messages", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      conversationId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "conversations",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      senderId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("messages");
+  },
+};

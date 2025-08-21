6b1783fced5528bbc4a568cd74e1f94ecdd165ab
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2025-08-21 11:00:09 +0300

    feat: add user, auth, and photo routes to Express app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import userRouter from './modules/users/routes/userRoutes.js';
+import authRouter from './modules/auth/routes/authRoutes.js';
+import photoRouter from './modules/users/routes/photoRoutes.js';
 
 dotenv.config();
 
@@ -15,3 +18,7 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
+app.use('/photo', photoRouter);    

fba1a07abc6a3d7ce95137e1f078b467b2ceabfa
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2025-08-21 10:59:48 +0300

    feat: add users and user_photos tables migration

---
+++ migrations/20250820150104-create-users-and-photos.js
@@ -0,0 +1,94 @@
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      surname: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      birthdate: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      sex: {
+        type: Sequelize.ENUM('ERKEK', 'KADIN'),
+        allowNull: true,
+      },
+      emailVerified: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      isPremium: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      profilePhoto: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()')
+      }
+    });
+
+    await queryInterface.createTable('user_photos', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isProfile: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('NOW()')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_photos');
+    await queryInterface.dropTable('users');
+  }
+};
